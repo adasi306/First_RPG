f72266977a47e9fbb287f55d2d64c559152d2c25
Adam <adasi306@gmail.com>
2024-07-09 16:32:07 +0200

    update

--- characters/enemies.py
+++ characters/enemies.py
@@ -1,7 +1,14 @@
 from bp import bpchar
 import random
 
-class skeleton(bpchar):
+
+class enemy(bpchar):
+    def death(self):
+        if self.health <= 0:
+            print("you've killed an enemy")
+
+
+class skeleton(enemy):
     def __init__(self, name, damage, health, maxhealth, armor, dodge_chance, crit_chance, speed, stress, status_effects):
         super().__init__(name, damage, health, maxhealth, armor, dodge_chance, crit_chance, speed, stress, status_effects)
 
